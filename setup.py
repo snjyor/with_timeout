bbabeee6bc1621a0f0309a18a6beae65c67eeeea
snjyor <snjyor@163.com>
2023-04-27 22:18:24 +0800

    0.0.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with io.open(path.join(abs_path, 'README.md'), encoding='utf-8') as f:
 setup(
     name='with_timeout',
     py_modules=['with_timeout'],
-    version='0.0.1',
+    version='0.0.4',
     description='模拟requests的timeout参数，使任意函数都拥有超时跳出的功能，防止访问超时阻塞影响代码的运行',
     author='Jeffrey Yang',
     author_email='snjyor@163.com',
