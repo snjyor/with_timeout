f52a55ad160fe700bd3eeb23877f18725775806b
snjyor <snjyor@163.com>
2023-04-27 22:13:42 +0800

    modifid again

--- setup.py
+++ setup.py
@@ -4,19 +4,20 @@ from os import path
 import io
 abs_path = path.abspath(path.dirname(__file__))
 with io.open(path.join(abs_path, 'README.md'), encoding='utf-8') as f:
-    longdesc = f.read()
+    readme_data = f.read()
 
 setup(
     name='with_timeout',
     py_modules=['with_timeout'],
-    version='0.1.7',
-    description='Conservatively convert html to markdown',
-    author='David Lönnhager',
-    author_email='dv.lnh.d@gmail.com',
-    url='https://github.com/dlon/html2markdown',
+    version='0.0.1',
+    description='模拟requests的timeout参数，使任意函数都拥有超时跳出的功能，防止访问超时阻塞影响代码的运行',
+    author='Jeffrey Yang',
+    author_email='snjyor@163.com',
+    url='https://github.com/snjyor/with_timeout',
     install_requires=[
         'beautifulsoup4'
     ],
-    long_description=longdesc,
-    long_description_content_type='text/x-rst',
+    license='MIT',
+    long_description=readme_data,
+    long_description_content_type='text/markdown',
 )
